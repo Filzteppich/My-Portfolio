25f95c1fd2f4de2d05a75ed4ea1758c5de08b193
Filzteppich <khang_136@yahoo.de>
2026-04-23 16:42:21 +0200

    Add new icons, styles, and about me section to the portfolio
    - Added various skill icons (CSS, Firebase, Git, Growth, HTML, JavaScript, Material Design, React, SCSS, Tailwind, TypeScript, Vue).
    - Introduced social icons for GitHub and LinkedIn with hover effects.
    - Created a new about me section with styling in about-me.css.
    - Updated landing page to include the new about me section and skills section.
    - Added skills section with descriptions and icons for technologies used.
    - Implemented JavaScript functions for icon hover effects.
    - Updated font paths in fonts.css.
    - Enhanced styles in landingpage.css and skills.css for better layout and design.
    - Added new CSS variables for color management.

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
-function init(){
-   return console.log('hallo welt');
 
+
+function init(){
+   initSkills()
+}
+
+function changeIcon(){
+   let iconRef = document.getElementById('githubIcon');
+   let radioIconRef = document.getElementById('radioIcon');
+      iconRef.src = "./imgs/icons/social-icons/GitHub-Hover.svg"
+      radioIconRef.style.display = "flex";   
+   };
+
+function resetIcon(){
+   let iconRef = document.getElementById('githubIcon');
+   let radioIconRef = document.getElementById('radioIcon');
+   iconRef.src = "./imgs/icons/social-icons/GitHub.svg"
+   radioIconRef.style.display = "none";
 }
 
 
+   
